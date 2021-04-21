69ab0e4e9806a8bb139c71f8d2e7e6cfbaf071e1
FabrizioMarongiu <78483709+FabrizioMarongiu@users.noreply.github.com>
2021-04-21 17:18:16 +0200

    Next, prev and circles

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const app = new Vue(
+    {
+        el: '#app',
+   
+    data : {
+        image: [
+            'https://picsum.photos/id/237/500/300',
+            'https://picsum.photos/500/300',
+            'https://picsum.photos/seed/picsum/500/300',
+            'https://picsum.photos/id/29/500/300',
+            'https://picsum.photos/id/1005/500/300',
+            'https://picsum.photos/id/1011/500/300',
+            'https://picsum.photos/id/112/500/300',
+        ],
+    
+        indice: 0,
+    },
+        methods:{
+            next(){
+                console.log(this.indice);
+                if(this.indice === (this.image.length) - 1){
+                    this.indice = 0;
+                }else{
+                    this.indice ++;
+                }
+            },
+            prev(){
+                console.log(this.indice)
+                if(this.indice === 0){
+                    this.indice = (this.image.length - 1);
+                }else{
+                    this.indice--;
+                }
+            },
+            circle(index){
+                this.indice = index;
+            },
+        },
+        
+
+        
+    
+ });
